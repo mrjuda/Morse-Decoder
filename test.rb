3d173ac944c120c3cef6bd474a047ab9be491b46
judah <87juda@gmail.com>
2022-08-24 18:07:07 -0300

    First ruby tests

--- test.rb
+++ test.rb
@@ -1,1 +1,17 @@
-puts("Hello ruby")
+def nameAndAge2(name,birthyear)
+  puts("I'm #{name}, I was born in #{birthyear}, so I'm #{2022-birthyear} years old")
+  # return result
+end
+
+# nameAndAge2("Judá", 1987)
+
+def oneMorseChar(str)
+  case str
+  when ".-"
+    puts("A")
+  else
+    puts("Not A")
+  end
+end
+
+oneMorseChar(".-s")
