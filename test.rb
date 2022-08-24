d4dc875ddd8b79aa6b6755f81e53cac00bf191a2
judah <87juda@gmail.com>
2022-08-24 18:14:12 -0300

    Add Morse alphabet to a case statement

--- test.rb
+++ test.rb
@@ -7,8 +7,60 @@ end
 
 def oneMorseChar(str)
   case str
+  when "   "
+    puts(" ")
   when ".-"
     puts("A")
+  when "-..."
+    puts("B")
+  when "-.-."
+    puts("C")
+  when "-.."
+    puts("D")
+  when "."
+    puts("E")
+  when "..-."
+    puts("F")
+  when "--."
+    puts("G")
+  when "...."
+    puts("H")
+  when ".."
+    puts("I")
+  when ".---"
+    puts("J")
+  when "-.-"
+    puts("K")
+  when ".-.."
+    puts("L")
+  when "--"
+    puts("M")
+  when "-."
+    puts("N")
+  when "---"
+    puts("O")
+  when ".--."
+    puts("P")
+  when "--.-"
+    puts("Q")
+  when ".-."
+    puts("R")
+  when "..."
+    puts("S")
+  when "-"
+    puts("T")
+  when "..-"
+    puts("U")
+  when "...-"
+    puts("V")
+  when ".--"
+    puts("W")
+  when "-..-"
+    puts("X")
+  when "-.--"
+    puts("Y")
+  when "--.."
+    puts("Z")
   else
     puts("Not A")
   end
