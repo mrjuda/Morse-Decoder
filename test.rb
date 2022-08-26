8a5410e89abb121577644c996f6f22d1f46c0e32
judah <87juda@gmail.com>
2022-08-26 19:31:26 -0300

    Create morse alphabet object

--- test.rb
+++ test.rb
@@ -1,62 +1,30 @@
-def oneMorseChar(str)
-  case str
-  when "   "
-    puts(" ")
-  when ".-"
-    puts("A")
-  when "-..."
-    puts("B")
-  when "-.-."
-    puts("C")
-  when "-.."
-    puts("D")
-  when "."
-    puts("E")
-  when "..-."
-    puts("F")
-  when "--."
-    puts("G")
-  when "...."
-    puts("H")
-  when ".."
-    puts("I")
-  when ".---"
-    puts("J")
-  when "-.-"
-    puts("K")
-  when ".-.."
-    puts("L")
-  when "--"
-    puts("M")
-  when "-."
-    puts("N")
-  when "---"
-    puts("O")
-  when ".--."
-    puts("P")
-  when "--.-"
-    puts("Q")
-  when ".-."
-    puts("R")
-  when "..."
-    puts("S")
-  when "-"
-    puts("T")
-  when "..-"
-    puts("U")
-  when "...-"
-    puts("V")
-  when ".--"
-    puts("W")
-  when "-..-"
-    puts("X")
-  when "-.--"
-    puts("Y")
-  when "--.."
-    puts("Z")
-  else
-    puts("Not A")
-  end
-end
+@morse_code_dictionary = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
 
-oneMorseChar("-- -.--   -. .- -- .")
+# oneMorseChar("-- -.--   -. .- -- .")
