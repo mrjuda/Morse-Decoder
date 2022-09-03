da523bdc75288bc77332e9328107ab0c8fc5e729
judah <87juda@gmail.com>
2022-09-03 17:18:11 -0300

    Add shop.rb file + Ship class

---
+++ ship.rb
@@ -0,0 +1,15 @@
+# ship.rb
+
+class Ship
+  ACELERATION = 1
+  def initialize
+    @position = { x: 0, y: 0 }
+    @speed = { x: 0, y: 0 }
+  end
+  def acceleration(direction)
+    # additional code
+  end
+  def move
+    # additional code
+  end
+end
