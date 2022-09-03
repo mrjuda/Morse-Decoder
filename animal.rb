46f366f8e9eb4b30caab7d5e426ea7024702cf85
judah <87juda@gmail.com>
2022-09-03 16:42:52 -0300

    Play with OOP

--- animal.rb
+++ animal.rb
@@ -1,20 +1,46 @@
 # Test ruby
 
 class Animal
-  def initialize(number_of_legs, name = 'Unknown')
+  def initialize(type, number_of_legs, name = 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
     @type = type
   end
 
-  def speak
-    'Bla bla bla'
+  def id
+    @id
   end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  # def name
+  #   @name
+  # end
+  # def name=(value)
+  #   @name = value
+  # end
+  # The last 6 lines could be replaced by:
+  # attr_reader :name
+  # attr_writer :name
+  # The last 2 lines could be replaced by:
+  attr_accessor :name
 end
 
-animal_1 = Animal.new(4, 'Rex')
-animal_2 = Animal.new(8)
+animal_1 = Animal.new("dog", 4, "Rex")
+animal_1 = Animal.new("dog", 4, "Rex")
+p animal_1.id
+p animal_1.type
+p animal_1.name
+p animal_1.number_of_legs
 
-animal_1.speak
-animal_2.speak
+animal_2 = Animal.new("cat", 8)
+p animal_2.name
+animal_2.name = "Fluffy"
+p animal_2.name
